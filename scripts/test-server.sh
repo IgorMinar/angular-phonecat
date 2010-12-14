7501c11c495f7fb7069abfaf7c51ab505c33f816
Igor Minar <iiminar@gmail.com>
2010-12-14 16:30:31 -0500

    display jstd server port

--- scripts/test-server.sh
+++ scripts/test-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 base_dir=`dirname $0`
+port=9876
 
-echo "Starting JSTD server..."
+echo "Starting JSTD server on port $port..."
 
-java -jar "$base_dir/../test/lib/jstestdriver/JsTestDriver.jar" --port 9876 --browserTimeout 20000
+java -jar "$base_dir/../test/lib/jstestdriver/JsTestDriver.jar" --port $port --browserTimeout 20000
