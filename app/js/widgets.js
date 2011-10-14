30c5111b4cb805a355ef7cd1ff20b1feff956c64
Misko Hevery <misko@hevery.com>
2011-10-14 16:21:21 -0700

    Brainstorming on configuration and testability

--- app/js/widgets.js
+++ app/js/widgets.js
@@ -1,10 +1,12 @@
-angular.widget('my:time', function(compileElement){
-  compileElement.css('display', 'block');
-  return function(linkElement){
-    function update(){
-      linkElement.text('Current time is: ' + new Date());
-      setTimeout(update, 1000);
-    }
-    update();
-  };
-});
+angular.module('PhoneCat-UI', ['PhoneCat'], ['$html', function($html){
+  $html.widget('my:time', function(compileElement){
+    compileElement.css('display', 'block');
+    return function(linkElement){
+      function update(){
+        linkElement.text('Current time is: ' + new Date());
+        setTimeout(update, 1000);
+      }
+      update();
+    };
+  });
+}]);
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,46 +1,39 @@
 describe('favorites', function(){
-  var scope, favorites, $cookieStore;
-
-  beforeEach(function(){
-    scope = angular.scope();
-    $cookieStore = scope.$inject('$cookieStore');
-    favorites = scope.$inject('favorites');
-  });
-
 
   describe('add', function() {
 
-    it('should add phones to favorites and store them in cookie store', function() {
-      expect($cookieStore.get('favorites')).toBeUndefined();
-      favorites.add('abc123');
-      expect($cookieStore.get('favorites')).toEqual({'abc123':true});
-    });
+    it('should add phones to favorites and store them in cookie store',
+      block(function($cookieStore, favarites) {
+        expect($cookieStore.get('favorites')).toBeUndefined();
+        favorites.add('abc123');
+        expect($cookieStore.get('favorites')).toEqual({'abc123':true});
+      }));
 
   });
 
 
   describe('remove', function() {
 
-    it('should remove phones from favorites', function() {
+    it('should remove phones from favorites', block(function(favorites, $cookieStore) {
       favorites.add('abc123');
       expect($cookieStore.get('favorites')).toEqual({"abc123":true});
 
       favorites.remove('abc123');
       expect($cookieStore.get('favorites')).toEqual({});
-    });
+    }));
 
   });
 
 
   describe('has', function() {
 
-    it('should return true if an item is favorite', function() {
+    it('should return true if an item is favorite', block(function(favorites) {
       expect(favorites.has('abc123')).toBe(false);
 
       favorites.add('abc123');
 
       expect(favorites.has('abc123')).toBe(true);
-    });
+    }));
 
   });
 
